84882791f2f610dcf0a0791cb3ac4a7e36c70e9e
C-Medina89 <m.masud@lbhospital.med.ly>
2025-11-17 22:04:47 +0000

    Task 6 done evaluating the model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.preprocess_data import convert_data, decode_data, clean_and_save_data
 from src.prepare_data import load_features_and_target
 from src.split_data import split_data
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 
 RANDOM_SEED = 42
@@ -44,10 +45,33 @@ print("X_test:", X_test.shape)
 print("y_train:", y_train.shape)
 print("y_test:", y_test.shape)
 
-
+# === TRAIN DECISION TREE ===
 model = DecisionTreeClassifier(random_state=RANDOM_SEED, criterion='entropy', max_depth=3)
 model.fit(X_train, y_train)
 
 print("Model trained!")
 print(f"Tree depth: {model.get_depth()}")
 print(f"Number of leaves: {model.get_n_leaves()}")
+
+# --- Make predictions on the test set ---
+y_pred = model.predict(X_test)
+
+
+# --- Evaluation metrics ---
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='macro')
+recall = recall_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+cm = confusion_matrix(y_test, y_pred)
+
+
+print("\nMODEL EVALUATION RESULTS")
+print("-------------------------")
+print(f"Accuracy:  {accuracy:.3f}")
+print(f"Precision: {precision:.3f}")
+print(f"Recall:    {recall:.3f}")
+print(f"F1 Score:  {f1:.3f}")
+
+print("\nConfusion Matrix:")
+print(cm)
